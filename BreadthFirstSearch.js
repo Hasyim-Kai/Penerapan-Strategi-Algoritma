c9e80e1818046764f78e31045fc1f8db1ae1b014
ichihatsu1 <ichihatsu1@gmail.com>
2022-03-31 10:42:16 +0700

    Feat : Add BreadthFirstSearch Algorithm

---
+++ BreadthFirstSearch.js
@@ -0,0 +1,48 @@
+const adjacencyList = new Map();
+const node = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+const edge = [
+    ['A', 'B'],
+    ['A', 'C'],
+    ['B', 'D'],
+    ['B', 'E'],
+    ['C', 'F'],
+    ['C', 'G']
+]
+
+const addNode = node => {
+    adjacencyList.set(node, [])
+}
+
+const addEdge = (origin, destination) => {
+    adjacencyList.get(origin).push(destination);
+    adjacencyList.get(destination).push(origin);
+}
+
+node.forEach(addNode);
+edge.forEach(edge => addEdge(...edge));
+
+// console.log(adjacencyList);
+
+function BreadthFirstSearch (start, targetDestination){
+    const visited = new Set(start)
+    const queue = [start]
+
+    while(queue.length > 0){
+        // console.log(queue)
+
+        const node = queue.shift()
+        const destinations = adjacencyList.get(node)
+
+        for(const destination of destinations){          
+            if(!visited.has(destination)){
+                visited.add(destination)
+                queue.push(destination)
+                if(destination === targetDestination){
+                    console.log(`found in node => ` + destination)
+                } else console.log(destination)                
+            }
+        }
+    }
+}
+
+BreadthFirstSearch('A', 'F')
