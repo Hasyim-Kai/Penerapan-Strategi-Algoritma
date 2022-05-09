170ed385446f01494e05baa9c7057e98d501a919
ichihatsu1 <ichihatsu1@gmail.com>
2022-05-09 17:16:57 +0700

    Feat : Add Brute Force Search

--- src/Logic/brute_force.ts
+++ src/Logic/brute_force.ts
@@ -1,6 +1,6 @@
 // Bubble sort
 
-function bblSort(array) {
+function bblSort(array: number[]) {
     for (var i = 0; i < array.length; i++) {
         // Last i elements are already in place
         for (var j = 0; j < (array.length - i - 1); j++) {
@@ -17,9 +17,9 @@ function bblSort(array) {
 
     let biggestNumber = array[array.length - 1], secondBiggestNumber = 0;
     // find secondBiggestNumber from behind
-    for (index = array.length - 1; index >= 0; index--) {
+    for (let index = array.length - 1; index >= 0; index--) {
         // once we found a number smaller than biggest Number, we assign it to variable and break the loop
-        if(array[index] < biggestNumber){ 
+        if (array[index] < biggestNumber) {
             secondBiggestNumber = array[index]
             break
         }
@@ -34,3 +34,14 @@ function bblSort(array) {
 var array = [234, 43, 55, 63, 1231, 6, 235, 547];
 // Now pass this array to the bblSort() function
 bblSort(array);
+
+export const bruteForceSearch = (arr: number[], target: number) => {
+    if (arr.includes(target)) {
+        for (let index = 0; index <= arr.length - 1; index++) {
+            console.log(index)
+            if (target === arr[index]) return index + 1
+        }        
+    } else {
+        return 0
+    }
+}
