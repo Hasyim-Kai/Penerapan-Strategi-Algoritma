af4389f6444b055bc025c17f093db28030286fa8
ichihatsu1 <ichihatsu1@gmail.com>
2022-05-09 17:17:21 +0700

    Feat : Add Divide & Conquer Search

--- src/Logic/divideNconquer.ts
+++ src/Logic/divideNconquer.ts
@@ -1,4 +1,5 @@
-function binarySearch(arr, target) {
+export default function binarySearch(arr : number[], target : number) {
+    let steps: number = 0;
     if (arr.includes(target)) {
         //start will be the first index & end will be last index 
         let start = 0, end = arr.length - 1;
@@ -7,9 +8,11 @@ function binarySearch(arr, target) {
         let guessIndex = Math.floor((end - start) / 2)
 
         while (end > start) {
+            ++steps
             console.log(`start index : ${start} | end index : ${end} | guessIndex index : ${guessIndex}`)
             if (arr[guessIndex] === target) {
-                return `found at index : ${guessIndex}`
+                return steps
+                // return `found at index : ${guessIndex} with ${steps} Steps`
             } else if (arr[guessIndex] > target) {
                 //this means our guess was too big. It also means we don't need the end-half of the array. 
                 //we reassign end to our guess. Now our array is halved. We also  reassign guess to the middle of the bottom half of 
@@ -28,9 +31,9 @@ function binarySearch(arr, target) {
             }
         }
     } else {
-        return 'Target Not Found in the given Array'
+        return 0
     }
 }
 
-let myArray = [12, 54, 23, 76, 14, 1, 64, 89, 90, 34, 111].sort((a, b) => a - b)
-console.log(binarySearch(myArray, 111))
+// let myArray = [12, 54, 23, 76, 14, 1, 64, 89, 90, 34, 111].sort((a, b) => a - b)
+// console.log(binarySearch(myArray, 111))
