5b76570c58773b4b9dbc975be93bd3861fca84a5
Kai-69 <ichihatsu1@gmail.com>
2022-05-26 10:55:30 +0700

    Feat : Add Sidebar Component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { Link, useLocation } from "react-router-dom";
+
+export default function Sidebar() {
+   const transitionStyle = `transition-all duration-300`;
+   const sidebarItemStyle = `p-2 hover:bg-white hover:text-blue-700 rounded-lg`;
+
+   // get the active Route path
+   let isLoginPage = useLocation().pathname === '/' ? true : false
+
+   return isLoginPage ? <></> : <nav className="w-52 bg-blue-600 text-white text-lg py-2 px-5 shadow-xl">
+      <ul>
+         <li className={`border-b border-white pb-5 text-7xl mb-5`}>
+            SA
+         </li>
+         <li className={`${sidebarItemStyle} ${transitionStyle}`}>
+            <Link to='fibonacci'>Fibonacci</Link>
+         </li>
+         <li className={`${sidebarItemStyle} ${transitionStyle}`}>
+            <Link to='find-number'>Find Number</Link>
+         </li>
+         <li className={`${sidebarItemStyle} ${transitionStyle}`}>
+            <Link to='money-return'>Money Return</Link>
+         </li>
+         <li className={`${sidebarItemStyle} ${transitionStyle}`}>
+            <Link to='/'>Logout</Link>
+         </li>
+      </ul>
+   </nav>
+}
